018a00bbe90eb60b7ef559eb27f99c1a1f3a3611
gorash-bolatu <cotwi10@yandex.ru>
2025-06-11 05:42:25 +0800

    разбил на файлы

---
+++ items.pas
@@ -0,0 +1,12 @@
+﻿unit Items;
+
+uses Inventory;
+
+const
+    itCola: Item = (name: 'Бутылка колы');
+    itCharger: Item = (name: 'Зарядка от ноутбука');
+    itHdd: Item = (name: 'Жёсткий диск');
+    itShard: Item = (name: 'Осколок зеркала');
+    itDog: Item = (name: 'Юпитер');
+
+end.
